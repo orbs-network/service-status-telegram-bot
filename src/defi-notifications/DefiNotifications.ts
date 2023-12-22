ef446d8dbc48d670b22b11d55bb2a770474134f8
Sukh Singh <sukh@orbs.com>
2023-12-22 11:44:40 +0000

    fixed defi response type

--- src/defi-notifications/DefiNotifications.ts
+++ src/defi-notifications/DefiNotifications.ts
@@ -11,10 +11,10 @@ export class DefiNotifications {
       const detectorResults = await fetch(
         'https://open-defi-notifications-detect.herokuapp.com/health'
       );
-      const detectorStatus = (await detectorResults.json()) as { Status: string };
+      const detectorStatus = (await detectorResults.json()) as { status: string };
       statuses.push({
         name: 'Detector',
-        status: detectorStatus.Status.substring(9, detectorStatus.Status.indexOf(',')).trim(),
+        status: detectorStatus.status,
       });
 
       const managerResults = await fetch(
@@ -27,10 +27,10 @@ export class DefiNotifications {
       });
 
       const l3Results = await fetch('https://odnp-l3-test-node.global.ssl.fastly.net/health');
-      const l3Status = (await l3Results.json()) as { status: string };
+      const l3Status = (await l3Results.json()) as { Status: string };
       statuses.push({
         name: 'Orbs L3',
-        status: l3Status.status,
+        status: l3Status.Status.substring(9, l3Status.Status.indexOf(',')).trim(),
       });
     } catch (err) {
       console.error('Error loading Defi Notifications statuses', err);
@@ -39,7 +39,7 @@ export class DefiNotifications {
   }
 
   static async report() {
-    let output = `📊 *${NotificationTypeNames[NotificationType.DefiNotifications]}}*\n\n`;
+    let output = `📊 *${NotificationTypeNames[NotificationType.DefiNotifications]}*\n\n`;
     try {
       const statuses = await DefiNotifications.loadStatuses();
       const tableOutput = statuses.map((status) => [
