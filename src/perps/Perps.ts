18a701f34d05b19a043046664bc2842ef1f587e3
Sukh Singh <sukh@orbs.com>
2024-05-03 16:49:20 +0100

    epsilon comparison

--- src/perps/Perps.ts
+++ src/perps/Perps.ts
@@ -103,8 +103,7 @@ export class Perps {
         const data = (await resp.json()) as PairExposureComparison[];
 
         data.forEach((d) => {
-          const exposure = d.quantityDelta * d.markPrice;
-          if (exposure > 0) {
+          if (d.quantityDelta > Number.EPSILON) {
             alerts.push({
               notificationType,
               alertType: PerpsAlert.PerpsExposure,
@@ -112,7 +111,7 @@ export class Perps {
               timestamp: new Date().getTime(),
               message: `🚨 *${NotificationTypeNames[notificationType]}* 🚨\n\n*${
                 d.symbol
-              }*: ${dollar.format(exposure)}`,
+              }*: ${dollar.format(d.quantityDelta * d.markPrice)}`,
             });
           }
         });
