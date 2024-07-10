8750f5b2788be425ad92f64169607e387b092f6e
Sukh Singh <sukh@orbs.com>
2024-07-10 18:31:53 +0100

    updated takers

--- src/twap/Twap.ts
+++ src/twap/Twap.ts
@@ -9,13 +9,8 @@ export class Twap {
     const takers: TakerStatus[] = [];
     try {
       const backupTakers = [
-        'https://twap-taker-1-05c260c20df9.herokuapp.com/health',
-        'https://twap-taker-2.herokuapp.com/health',
-        'https://twap-taker-3-8f650e133459.herokuapp.com/health',
-        'https://twap-taker-4-77af356494f9.herokuapp.com/health',
-        'https://twap-taker-5-0d0999686c9f.herokuapp.com/health',
-        'https://twap-taker-6-4c613d1380ac.herokuapp.com/health',
-        'http://198.20.104.2:9000/health',
+        'http://142.234.204.136:9000/health',
+        'http://178.162.169.6:9000/health',
       ];
 
       const results = await Promise.allSettled(backupTakers.map((url) => fetch(url)));
